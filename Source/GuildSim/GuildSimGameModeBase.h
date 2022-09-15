f776717c54c6b80f10f45b905f41f968e1cd0ad0
RollAllStudio <101832245+RollAllStudio@users.noreply.github.com>
2022-09-15 19:00:38 +0200

    Init
    Init

---
+++ Source/GuildSim/GuildSimGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GuildSimGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GUILDSIM_API AGuildSimGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
